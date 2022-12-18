bf3a45d794a9bfffcab4f73979d2f20ada5ff59e
ERENEJ <eerenatci@gmail.com>
2022-12-18 15:04:47 +0300

    fixed typo at Prerequisites.h

--- SeniorProject/IndexBuffer.h
+++ SeniorProject/IndexBuffer.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <d3d11.h>
-#include "Prerequisistes.h"
+#include "Prerequisites.h"
 
 class IndexBuffer
 {
---
+++ SeniorProject/Mesh.cpp
@@ -0,0 +1,114 @@
+
+#include "Mesh.h"
+
+#define TINYOBJLOADER_IMPLEMENTATION
+#include <tiny_obj_loader.h>
+
+#include <locale>
+#include <codecvt>
+
+#include "GraphicsEngine.h"
+#include "VertexMesh.h"
+
+Mesh::Mesh(const wchar_t* full_path) : Resource(full_path)
+{
+	tinyobj::attrib_t attribs;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+
+	std::string warn;
+	std::string err;
+
+	//from locale file converting to ascii string 
+	std::string inputfile = std::wstring_convert<std::codecvt_utf8<wchar_t>>().to_bytes(full_path);
+
+
+	//loadObj file path only accpets ascii strings
+	bool res = tinyobj::LoadObj(&attribs, &shapes, &materials, &warn, &err, inputfile.c_str());
+
+	if (!err.empty())throw std::exception("Mesh not created successfully");
+
+	if (!res)throw std::exception("Mesh not created successfully");
+
+
+	//to hold the vertices and texturecoordinates from tinjobj loader methods
+	//for vertex buffer
+	std::vector<VertexMesh> list_vertices;
+
+	//to hold indices for index buffer
+	std::vector<unsigned int> list_indices;
+
+
+	//TODO this logic Currently supports only 1 shape
+	//iterate over shapes 
+	for (size_t s = 0; s < shapes.size(); s++)
+	{
+		//every shape must start index 0
+		size_t index_offset = 0;
+
+		//to speed up the insertion operation of vector  
+		list_vertices.reserve(shapes[s].mesh.indices.size());
+		list_indices.reserve(shapes[s].mesh.indices.size());
+
+		//loop through faces of shape
+		for (size_t f = 0; shapes[s].mesh.num_face_vertices.size(); f++)
+		{
+			//store the face amount of model (a.k.a total triangle count)
+			unsigned char num_face_verts = shapes[s].mesh.num_face_vertices[f];
+
+			//each face consists multiple Vertices in this case Triangle
+			//that means we need to work on 3 Vertices
+			for (unsigned char v = 0; v < num_face_verts; v++)
+			{
+				tinyobj::index_t index = shapes[s].mesh.indices[index_offset + v];
+
+				//vertex are stored side by side 
+				//Obj1Vertex | Obj1Vertex1 | Obj1Vertex2 | Obj2Vertex0 | Obj2Vertex1 .....
+				tinyobj::real_t vx = attribs.vertices[index.vertex_index * 3 + 0];
+				tinyobj::real_t vy = attribs.vertices[index.vertex_index * 3 + 1];
+				tinyobj::real_t vz = attribs.vertices[index.vertex_index * 3 + 2];
+
+				//Texture coordinate
+				// same logic texture coordinates stored side by side
+				tinyobj::real_t tx = attribs.texcoords[index.texcoord_index * 2 + 0];
+				tinyobj::real_t ty = attribs.texcoords[index.texcoord_index * 2 + 1];
+
+				//VertexMesh is customly created just to hold these values
+				VertexMesh vertex(Vector3D(vx, vy, vz), Vector2D(tx, ty));
+				list_vertices.push_back(vertex);
+
+				//push the indices for index buffer
+				list_indices.push_back(index_offset + v);
+
+			}
+
+			index_offset += num_face_verts;
+		}
+	}
+
+	void* shader_byte_code = nullptr;
+	size_t size_shader = 0;
+
+	GraphicsEngine::get()->getVertexMeshLayoutShaderByteCodeAndSize(&shader_byte_code, &size_shader);
+
+	m_vertex_buffer = GraphicsEngine::get()->getRenderSystem()->createVertexBuffer(
+		&list_vertices[0], sizeof(VertexMesh), list_vertices.size(), shader_byte_code, (UINT)size_shader);
+
+	m_index_buffer = GraphicsEngine::get()->getRenderSystem()->createIndexBuffer(&list_indices[0],
+		(UINT)list_indices.size());
+
+}
+
+Mesh::~Mesh()
+{
+}
+
+const VertexBufferPtr& Mesh::getVertexBuffer()
+{
+	return m_vertex_buffer;
+}
+
+const IndexBufferPtr& Mesh::getIndexBuffer()
+{
+	return m_index_buffer;
+}
